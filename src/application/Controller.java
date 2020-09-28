98725e20a63f73ce85529dabeb89b989d62857b9
robi-7 <56583192+robi-7@users.noreply.github.com>
2020-09-28 17:08:56 +0200

    Metoder för punkt 1

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -35,6 +35,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import tableModels.Table1;
 import database.DataAccessLayer;
 
 public class Controller implements Initializable {
@@ -43,15 +44,200 @@ public class Controller implements Initializable {
 	private DataAccessLayer database;
 
 
+    @FXML
+    private TableView<Table1> tbl1;
+
+    @FXML
+    private TableColumn<Table1, String> tbl1Col1;
+
+    @FXML
+    private TableColumn<Table1, String> tbl1Col2;
+
+    @FXML
+    private TableColumn<Table1, String> tbl1Col3;
+
+    @FXML
+    private TableColumn<Table1, String> tbl1Col4;
+
+    @FXML
+    private TableColumn<Table1, String> tbl1Col5;
+   
+    @FXML
+    private Button btnEmpInfo;
+
+    @FXML
+    private Button btnEmpRel;
+
+    @FXML
+    private Button btnEmpAbs;
+
+    @FXML
+    private Button btnEmpSta;
+
+    @FXML
+    private Button btnEmpQua;
+
+    @FXML
+    private Button btnEmpPor;
 
 	//INITIALIZATOR
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
-		
+		tbl1Col1.setCellValueFactory(new PropertyValueFactory<Table1,String>("col1"));
+		tbl1Col2.setCellValueFactory(new PropertyValueFactory<Table1,String>("col2"));
+		tbl1Col3.setCellValueFactory(new PropertyValueFactory<Table1,String>("col3"));
+		tbl1Col4.setCellValueFactory(new PropertyValueFactory<Table1,String>("col4"));
+		tbl1Col5.setCellValueFactory(new PropertyValueFactory<Table1,String>("col5"));
+		try {
+			database = new DataAccessLayer();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	@FXML
+	public void getEmployeeInfo() {
+		try {
+			ResultSet rs = database.getEmployeeInfo();
+			ObservableList<Table1> data = FXCollections.observableArrayList();
+			while (rs.next()) {
+				String col1 = rs.getString(1);
+				String col2 = rs.getString(2);
+				String col3 = rs.getString(3);
+				String col4 = rs.getString(4);
+				String col5 = rs.getString(5);
+				data.add(new Table1(col1, col2, col3, col4, col5));
+			}
+			tbl1.setItems(data);
+			tbl1Col1.setText("First Name");
+			tbl1Col2.setText("Last Name");
+			tbl1Col3.setText("Job Title");
+			tbl1Col4.setText("Address");
+			tbl1Col5.setText("E-Mail");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	@FXML
+	public void getEmployeeRelative() {
+		try {
+			ResultSet rs = database.getEmployeeRelative();
+			ObservableList<Table1> data = FXCollections.observableArrayList();
+			while (rs.next()) {
+				String col1 = rs.getString(1);
+				String col2 = rs.getString(2);
+				String col3 = rs.getString(3);
+				String col4 = rs.getString(4);
+				String col5 = rs.getString(5);
+				data.add(new Table1(col1, col2, col3, col4, col5));
+			}
+			tbl1.setItems(data);	
+			tbl1Col1.setText("Emp. No");
+			tbl1Col2.setText("Relative Code");
+			tbl1Col3.setText("First Name");
+			tbl1Col4.setText("Last Name");
+			tbl1Col5.setText("Birth Date");
+			
+		} catch (SQLException e) {
+			
+		}
 	}
 
+	@FXML
+	public void getEmployeeAbsence() {
+		try {
+			ResultSet rs = database.getEmployeeAbsence();
+			ObservableList<Table1> data = FXCollections.observableArrayList();
+			while (rs.next()) {
+				String col1 = rs.getString(1);
+				String col2 = rs.getString(2);
+				String col3 = rs.getString(3);
+				String col4 = rs.getString(4);
+				String col5 = rs.getString(5);
+				data.add(new Table1(col1, col2, col3, col4, col5));
+			}
+			tbl1.setItems(data);	
+			tbl1Col1.setText("Entry No");
+			tbl1Col2.setText("Emp. No");
+			tbl1Col3.setText("From Date");
+			tbl1Col4.setText("To Date");
+			tbl1Col5.setText("Description");
+		} catch (SQLException e) {
+			
+		}
+	}
 	
+	@FXML
+	public void getEmployeeQualification() {
+		try {
+			ResultSet rs = database.getEmployeeQualification();
+			ObservableList<Table1> data = FXCollections.observableArrayList();
+			while (rs.next()) {
+				String col1 = rs.getString(1);
+				String col2 = rs.getString(2);
+				String col3 = rs.getString(3);
+				String col4 = rs.getString(4);
+				String col5 = rs.getString(5);
+				data.add(new Table1(col1, col2, col3, col4, col5));
+			}
+			tbl1.setItems(data);	
+			tbl1Col1.setText("Emp. No");
+			tbl1Col2.setText("From Date");
+			tbl1Col3.setText("To Date");
+			tbl1Col4.setText("Description");
+			tbl1Col5.setText("Company");
+		} catch (SQLException e) {
+			
+		}
+	}
+	
+	@FXML
+	public void getEmployeeStatsGroup() {
+		try {
+			ResultSet rs = database.getEmployeeStatsGroup();
+			ObservableList<Table1> data = FXCollections.observableArrayList();
+			while (rs.next()) {
+				String col1 = rs.getString(1);
+				String col2 = rs.getString(2);
+				String col3 = rs.getString(3);
+				String col4 = "";
+				String col5 = "";
+				data.add(new Table1(col1, col2, col3, col4, col5));
+			}
+			tbl1.setItems(data);	
+			tbl1Col1.setText("Timestamp");
+			tbl1Col2.setText("Code");
+			tbl1Col3.setText("Description");
+			tbl1Col4.setText("");
+			tbl1Col5.setText("");
+		} catch (SQLException e) {
+			
+		}
+	}
+	
+	@FXML
+	public void getEmployeePortalSetup() {
+		try {
+			ResultSet rs = database.getEmployeePortalSetup();
+			ObservableList<Table1> data = FXCollections.observableArrayList();
+			while (rs.next()) {
+				String col1 = rs.getString(1);
+				String col2 = rs.getString(2);
+				String col3 = rs.getString(3);
+				String col4 = rs.getString(4);
+				String col5 = rs.getString(5);
+				data.add(new Table1(col1, col2, col3, col4, col5));
+			}
+			tbl1.setItems(data);	
+			tbl1Col1.setText("First Name");
+			tbl1Col2.setText("Last Name");
+			tbl1Col3.setText("Job Title");
+			tbl1Col4.setText("Address");
+			tbl1Col5.setText("E-Mail");
+		} catch (SQLException e) {
+			
+		}
+	}
 
 
 }
--- src/database/DataAccessLayer.java
+++ src/database/DataAccessLayer.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 
 public class DataAccessLayer {
 	
-	String url = "jdbc:sqlserver://localhost:1433;database=Assignment1;";
+	String url = "jdbc:sqlserver://localhost:1433;database=Assignment2;";
 	String loginName = "sa";
 	String password = "AlexandraAmarOgnjenRobert1!";
 	Connection con;
@@ -19,5 +19,40 @@ public class DataAccessLayer {
 		con = DriverManager.getConnection(url,loginName,password);
 	}
 	
-	
-}
+	public ResultSet getEmployeeInfo() throws SQLException {
+		String query = "SELECT [First Name], [Last Name], [Job Title], [Address], [E-Mail] FROM [CRONUS Sverige AB$Employee]";
+		PreparedStatement ps = con.prepareStatement(query);
+		ResultSet rs = ps.executeQuery();
+		return rs;
+	}
+	public ResultSet getEmployeeRelative() throws SQLException {
+		String query = "SELECT [Employee No_], [Relative Code], [First Name], [Last Name], [Birth Date] FROM [CRONUS Sverige AB$Employee Relative]";
+		PreparedStatement ps = con.prepareStatement(query);
+		ResultSet rs = ps.executeQuery();
+		return rs;
+	}
+	public ResultSet getEmployeeAbsence() throws SQLException {
+		String query = "SELECT [Entry No_], [Employee No_], [From Date], [To Date], [Description] FROM [CRONUS Sverige AB$Employee Absence]";
+		PreparedStatement ps = con.prepareStatement(query);
+		ResultSet rs = ps.executeQuery();
+		return rs;
+	}
+	public ResultSet getEmployeeQualification() throws SQLException {
+		String query = "SELECT [Employee No_], [From Date], [To Date], Description, Institution_Company FROM [CRONUS Sverige AB$Employee Qualification]";
+		PreparedStatement ps = con.prepareStatement(query);
+		ResultSet rs = ps.executeQuery();
+		return rs;
+	}
+	public ResultSet getEmployeeStatsGroup() throws SQLException {
+		String query = "SELECT * FROM [CRONUS Sverige AB$Employee Statistics Group]";
+		PreparedStatement ps = con.prepareStatement(query);
+		ResultSet rs = ps.executeQuery();
+		return rs;
+	}
+	public ResultSet getEmployeePortalSetup() throws SQLException {
+		String query = "SELECT [First Name], [Last Name], [Job Title], [Address], [E-Mail] FROM [CRONUS Sverige AB$Employee]";
+		PreparedStatement ps = con.prepareStatement(query);
+		ResultSet rs = ps.executeQuery();
+		return rs;
+	}
+}	
